7dc09133795a45df4855ce51bd9aba9bd88c36e4
PLAYNET\sitnikava-p <palina.sitnikava@vokigames.com>
2025-04-24 15:11:48 +0200

    Правка фикстуры

--- conftest.py
+++ conftest.py
@@ -6,13 +6,6 @@ def calc():
     return BasicCalc()
 
 @pytest.fixture(scope="function")
-def new_calc_with_memory():
-    calc = NewCalc()
-    for i in range(-100, 101, 100):
-        calc.memo_plus(i)
-    return calc
-
-@pytest.fixture(scope="function")
 def new_calc_with_memory(tmp_path, monkeypatch):
     memory_file_path = tmp_path / "memory.txt"
     monkeypatch.setattr("qa_automation_cource.calculator.NewCalc.memory_file", str(memory_file_path))
