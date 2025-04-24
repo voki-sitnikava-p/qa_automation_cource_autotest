a97728bad30ef53af79b67474dd68e304453bf10
PLAYNET\sitnikava-p <palina.sitnikava@vokigames.com>
2025-04-24 15:10:25 +0200

    Правка фикстуры (добавила tmp_path т.к. падает ошибка при запуске тестов в несколько потоков)

--- conftest.py
+++ conftest.py
@@ -12,4 +12,12 @@ def new_calc_with_memory():
         calc.memo_plus(i)
     return calc
 
+@pytest.fixture(scope="function")
+def new_calc_with_memory(tmp_path, monkeypatch):
+    memory_file_path = tmp_path / "memory.txt"
+    monkeypatch.setattr("qa_automation_cource.calculator.NewCalc.memory_file", str(memory_file_path))
+    calc = NewCalc()
+    for i in range(-100, 101, 100):
+        calc.memo_plus(i)
+    return calc
 
